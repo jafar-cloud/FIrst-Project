c949c1ec2528a0e1735a5b6573de05faecdf3766
Jafar <abc1234.ax1@gmail.com>
2024-12-31 16:43:52 +0500

    features of print

---
+++ class2.py
@@ -0,0 +1,11 @@
+print('Jafar')
+print(12)
+print('akash','jafar',12)
+print('akash kumar roy\n'*10)
+print('hi',end=' ')
+print('jafar',end=' ')
+print('akash',end=' ')
+num1 = 12
+num2 = 13
+print('\nsum of',num1,'and',num2,'is : ',num1+num2)
+print(f'sum of {num1} and {num2} is : {num1+num2}')
