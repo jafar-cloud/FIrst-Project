bfd7ef98b97b24b329600b0fc9502a2618521c0d
Jafar <abc1234.ax1@gmail.com>
2024-12-31 20:01:54 +0500

    birthday dates

---
+++ project1.py
@@ -0,0 +1,10 @@
+jafarDOB = '31 May 2012'
+akashDOB = '12 Feb 1994'
+prashantDOB = '1 Jan 2024'
+julianDOB = '31 March 1985'
+muhammadDOB = '24 Aug 2024'
+print(f'jafar date of birth is {jafarDOB}')
+print(f'akash date of birth is {akashDOB}')
+print(f'prashant date of birth is {prashantDOB}')
+print(f'julian date of birth is {julianDOB}')
+print(f'muhammad date of birth is {muhammadDOB}')
