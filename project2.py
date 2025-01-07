15544f06492bd944370b5c49690899d969d219df
Jafar <abc1234.ax1@gmail.com>
2025-01-07 17:22:05 +0500

    project 2

---
+++ project2.py
@@ -0,0 +1,13 @@
+a = int(input('Enter First Number: '))
+b = int(input('Enter Second Number: '))
+sum = a + b
+dif = a - b
+prod = a * b
+divi = a / b
+print(f'Sum Of {a} and {b} is: {sum}')
+print(f'Difference Between {a} and {b} is: {dif}')
+print(f'Product Of {a} and {b} is: {prod}')
+print(f'Quotient Of {a} and {b} is: {divi}')
+# doing this for extra credit
+print('I am doing this for extra credit:')
+print(f'{a} raised to power {b} is: {a ** b}')
