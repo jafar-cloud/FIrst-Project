6631cb09eb9628e9fb6a6b68aa99f3dfa9527df0
Jafar <abc1234.ax1@gmail.com>
2025-01-20 17:03:42 +0500

    project of ASCII Values

---
+++ advanced_operators/project6(advancedoperators).py
@@ -0,0 +1,13 @@
+print(f'ASCII Value of {'z'} Is: {ord('z')}')
+print(f'ASCII Value of {'a'} Is: {ord('a')}')
+print(f'ASCII Value of {'b'} Is: {ord('b')}')
+print(f'ASCII Value of {'2'} Is: {ord('2')}')
+print(f'ASCII Value of {'c'} Is: {ord('c')}')
+print(f'ASCII Value of {'d'} Is: {ord('d')}')
+print(f'ASCII Value of {'3'} Is: {ord('3')}')
+print(f'ASCII Value of {'A'} Is: {ord('A')}')
+print(f'ASCII Value of {'B'} Is: {ord('B')}')
+print(f'ASCII Value of {'4'} Is: {ord('4')}')
+print(f'ASCII Value of {'C'} Is: {ord('C')}')
+
+
